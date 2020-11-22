d10fccbe2b7df00d6a09ce9dd992599862301dcc
anubhavcu <anubhav342y@gmail.com>
2020-11-22 07:55:44 +0530

    -created a dummy product list -added Homescreen page which displays list of products - added a Product component which is a card displaying product from   dummy products.js -modifications with ui for look and feel in header -changed favicon.ico

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">ShopNow!</Navbar.Brand>
+          <Navbar.Brand href="/">ShopNow !</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
